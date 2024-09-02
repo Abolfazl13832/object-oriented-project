8c7c84892fddf667616aa4a24c1552bc3ac50ea6
web-test <alypwr573@gmail.com>
2024-09-02 12:08:00 -0700

    create sell and rent class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from user import User
 from random import choice
 from estate import Apartment, House
 from region import Region
+from adventisment import ApartmentSell
 FRIST_NAME = ['Ali','Reza','Mahdi']
 LAST_NAME = ['Alipour','Rezai','Mahdavi']
 MOBILES = ['09929399646','09149532618','09145361568' , '09143598388','09389286531']
@@ -23,7 +24,6 @@ if __name__ == "__main__":
         region = reg1,
         address = "some text",
     )
-    print(apt1.show_description())
     reg2=Region("R2")
     house = House(
         user=User.object_list[2],area=800,rooms_count=6,build_year=1400,
@@ -42,5 +42,16 @@ if __name__ == "__main__":
         address = "some text",
 
     )
-    print(house.show_description())
-    # print(house1.show_description())
+
+    ape2=ApartmentSell(
+        user=User.object_list[0],area=80,rooms_count=2,build_year=1393,
+        has_elevator=True,
+        has_parking=True,
+        floor=2,
+        region = reg1,
+        address = "some text",
+        price_per_meter = 10,
+        discountable = True,
+        convertable = False
+    )
+    print(ape2.show_detail())
